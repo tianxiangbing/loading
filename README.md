08c59ce58a68241b3d39ef0681f001dc5a9eaeef
tianxiangbing <55342775@qq.com>
2015-08-06 16:07:23 +0800

    加载loading动画的实现

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # loading
 loading...
+效果图如下：
+![loading](example/loading.jpg)
+
+**[DEMO请点击这里查看.](http://www.lovewebgames.com/jsmodule/loading.html "loading demo")**
+#调用示例
+html:
+
+	<p>
+		<input type="button" id="loading1" value="loading我自己">
+		<input type="button" id="loading2" value="loading下面这个div">
+		<input type="button" id="loading3" value="loading全屏">
+	</p>
+	<div id="loading-content" style="width:300px;height:200px;border:1px solid #ccc;background-color:#f2f2f2;">这是个内容的例子</div>
+	<script src="../src/jquery-1.11.2.js"></script>
+	<script src="../src/loading.js"></script>
+js:
+
+	//loading我自己
+	$('#loading1').click(function(){
+		var load = new Loading();
+		load.init({
+			target: this
+		});
+		load.start();
+		setTimeout(function() {
+			load.stop();
+		}, 3000)
+	});
+	//loading下面这个div
+	$('#loading2').click(function(){
+		var load = new Loading();
+		load.init({
+			target: "#loading-content"
+		});
+		load.start();
+		setTimeout(function() {
+			load.stop();
+		}, 3000)
+	});
+	//loading全屏
+	$('#loading3').click(function(){
+		var load = new Loading();
+		load.init();
+		load.start();
+		setTimeout(function() {
+			load.stop();
+		}, 30000)
+	});
+#API
+##属性
+###target:string||dom
+	需要显示loading的节点，不传值时显示全屏的loading
+##方法
+###start:function()
+	开始loading
+###stop:function()
+	结束loading
+##事件
+###stop
+	target的stop事件触发时，结束loading. 如
+	$('html').trigger('stop')会结束全屏的loading动画.
---
+++ src/loading.js
@@ -0,0 +1,79 @@
+/*
+ * Created with Sublime Text 3.
+ * license: http://www.lovewebgames.com/jsmodule/index.html
+ * github: https://github.com/tianxiangbing/format-number
+ * User: 田想兵
+ * Date: 2015-08-05
+ * Time: 11:27:55
+ * Contact: 55342775@qq.com
+ */
+;
+(function(root, factory) {
+	//amd
+	if (typeof define === 'function' && define.amd) {
+		define(['$'], factory);
+	} else if (typeof exports === 'object') { //umd
+		module.exports = factory();
+	} else {
+		root.Loading = factory(window.Zepto || window.jQuery || $);
+	}
+})(this, function($) {
+	var Loading = function() {};
+	Loading.prototype = {
+		loadingTpl: '<div class="ui-loading"><div class="ui-loading-mask"></div><i></i></div>',
+		stop: function() {
+			var content = $(this.target);
+			this.loading.remove();
+		},
+		start: function() {
+			var _this = this;
+			var target = _this .target;
+			var content = $(target);
+			var loading = this.loading;
+			if (!loading) {
+				loading = $(_this.loadingTpl);
+				$('body').append(loading);
+			}
+			this.loading = loading;
+			var ch = $(content).outerHeight();
+			var cw = $(content).outerWidth();
+			if ($(target)[0].tagName == "HTML") {
+				ch = Math.max($(target).height(), $(window).height());
+				cw = Math.max($(target).height(), $(window).width());
+			}
+			console.log(cw,ch)
+			loading.height(ch).width(cw);
+			loading.find('div').height(ch).width(cw);
+			if (ch < 100) {
+				loading.find('i').height(ch).width(ch);
+			}
+			var offset = $(content).offset();
+			loading.css({
+				top: offset.top,
+				left: offset.left
+			});
+			var icon = loading.find('i');
+			var h = target ? ch : ch + $(window).scrollTop();
+			var w = target ? cw : cw + $(window).scrollLeft();
+			var top = (h - icon.height()) / 2;
+			var left = (w - icon.width()) / 2;
+			icon.css({
+				top: top,
+				left: left
+			})
+		},
+		init: function(settings) {
+			settings = settings || {};
+			this.loadingTpl = settings.loadingTpl || this.loadingTpl;
+			this.target = settings.target || 'html';
+			this.bindEvent();
+		},
+		bindEvent: function() {
+			var _this = this;
+			$(this.target).on('stop', function() {
+				_this.stop();
+			});
+		}
+	}
+	return Loading;
+});
